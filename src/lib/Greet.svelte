aae0f08c9bbce37d6fec96c7236e8d6f9bea1eca
francois <fgg67@hotmail.fr>
2023-01-28 15:19:58 +0100

    file get

--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,16 +1,38 @@
-<script>
-  import { invoke } from '@tauri-apps/api/tauri'
+<script lang="ts">
+import { invoke } from '@tauri-apps/api/tauri'
+import { onMount } from 'svelte';
 
-  let name = ''
-  let greetMsg = ''
+let name = ''
+let greetMsg = ''
 
-  async function greet() {
-    greetMsg = await invoke('greet', { name })
-  }
+async function greet() {
+  greetMsg = await invoke('greet', { name })
+};
+
+onMount(() => {
+let input = document.getElementById("file-input");
+
+input?.addEventListener("change", function() {
+    console.log("file changed");
+    let files = input.files;
+
+    let file = files[0];
+    let reader = new FileReader();
+    reader.onload = function() {
+      let content = reader.result;
+      console.log(content);
+      }
+      reader.readAsText(file);
+    });
+
+});
 </script>
 
 <div>
-  <input id="greet-input" placeholder="Enter a name..." bind:value="{name}" />
-  <button on:click="{greet}">Greet</button>
-  <p>{greetMsg}</p>
+<input id="greet-input" placeholder="Enter a name..." bind:value="{name}" />
+<button on:click="{greet}">Greet</button>
+<p>{greetMsg}</p>
+<!-- <button id="file-input"> -->
+<input type="file" id="file-input" multiple>
+<!-- </button> -->
 </div>
